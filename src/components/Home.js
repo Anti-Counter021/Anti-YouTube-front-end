a006f38a6ff25a648154d9ebf06c4b314dc300b0
counter0021 <ara123rus123ara@gmail.com>
2021-08-07 13:39:34 +0300

    Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,38 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
+import {Container, Row} from "react-bootstrap";
+
+import VideoCard from "./VideoCard";
 import Navigation from "./Navigation";
+import WithServices from "./WithService";
 
-const Home = () => {
 
-    return (<Navigation/>);
+const Home = ({Service}) => {
+
+    const [videos, setVideos] = useState([]);
+
+    useEffect(async () => {
+        await Service.videos()
+            .then(res => setVideos(res))
+            .catch(error => console.log(error));
+    }, []);
+
+    return (
+        <>
+            <Navigation/>
+            <Container>
+                <h1 className="text-center">Home page</h1>
+                <Row>
+                    {
+                        videos.length ? (
+                            videos.map(video => (<VideoCard key={video.id} video={video}/>))
+                        ) : null
+                    }
+                </Row>
+            </Container>
+        </>
+    );
 
 };
 
-export default Home;
+export default WithServices()(Home);
